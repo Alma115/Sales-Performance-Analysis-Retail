32d154e0630a6a618024ba914072d2ad70d4e1ba
Almaidah <84564928+Alma115@users.noreply.github.com>
2024-10-25 21:26:36 +0700

    Add files via upload

---
+++ Overall Performance by Year.sql
@@ -0,0 +1,17 @@
+Overall
+Performance by
+Year
+
+SELECT
+YEAR(order_date) AS years,
+SUM(sales) AS sales,
+COUNT(order_id) AS number_of_order
+FROM
+dqlab_sales_store
+WHERE
+order_status = 'order finished' AND
+YEAR(order_date) BETWEEN 2009 AND 2012
+GROUP BY
+years
+ORDER BY
+years;
